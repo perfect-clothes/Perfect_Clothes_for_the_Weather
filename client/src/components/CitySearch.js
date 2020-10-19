801a26fb3fdf09f15a47473df59ba11d1e6a4883
zooneon <snsxhdz@naver.com>
2020-10-19 19:19:13 +0900

    [Etc] Modify css

--- client/src/components/CitySearch.js
+++ client/src/components/CitySearch.js
@@ -13,25 +13,27 @@ const CitySearchBlock = styled.div`
 const StyledInput = styled.input `
     border: none;
     outline: none;
-    width: 200px;
+    width: 250px;
     height: 40px;
     padding: 0px 10px;
-    border-radius: 3px;
+    border-radius: 5px 0px 0px 5px;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1); 
 `;
 
 const StyledButton = styled.button `
     background: #686d76;
     outline: none;
     border: none;
-    border-radius: 3px;
+    border-radius: 0px 5px 5px 0px;
     color: white;
-    padding: 10px 10px;
+    padding: 10px 20px;
     height: 40px;
     cursor: pointer;
     &:hover {
         background: #373a40;
     }
     transition: 0.1s background;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1); 
 `;
 
 const CitySearch = () => {
