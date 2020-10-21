4f73a75189c653b0855e5f54288d451778f8a76d
zooneon <snsxhdz@naver.com>
2020-10-21 17:40:05 +0900

    [Add] Add allWeather api
    오늘의 모든 날씨를 받아오는 api 추가

--- client/src/lib/api/loadWeather.js
+++ client/src/lib/api/loadWeather.js
@@ -1,4 +1,7 @@
 import client from "./client";
 
-
+//현재 날씨 불러옴
 export const load = ({latitude, longitude}) => client.post('/api/curWeather', {latitude, longitude});
+
+//오늘의 모든 날씨 불러옴
+export const loadAllWeather = ({latitude, longitude}) => client.post('/api/allWeather', {latitude, longitude});
