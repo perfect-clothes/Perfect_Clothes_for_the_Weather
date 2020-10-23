31a8498e919a34194c21c14033e79d8d1c309e53
zooneon <snsxhdz@naver.com>
2020-10-23 12:38:33 +0900

    [Add] Add news modules
    loadNews api를 이용한 데이터 요청 및 뉴스 데이터를 관리하기 위한 모듈 작성

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -2,14 +2,16 @@ import {combineReducers} from "redux";
 import {all} from 'redux-saga/effects';
 import weather, {weatherSaga} from "./weather";
 import allWeather, {allWeatherSaga} from "./allWeather";
+import news, {newsSaga} from "./news";
 
 const rootReducer = combineReducers({
     weather,
     allWeather,
+    news,
 });
 
 export function* rootSaga() {
-    yield all([weatherSaga(), allWeatherSaga()]);
+    yield all([weatherSaga(), allWeatherSaga(), newsSaga()]);
 }
 
 export default rootReducer;
---
+++ client/src/modules/news.js
@@ -0,0 +1,53 @@
+import {createAction, handleActions} from 'redux-actions';
+import {takeLatest, put, call} from 'redux-saga/effects';
+import * as loadNewsAPI from '../lib/api/loadNews';
+
+const LOAD_NEWS = 'news/LOAD_NEWS';
+const LOAD_NEWS_SUCCESS = 'news/LOAD_NEWS_SUCCESS';
+const LOAD_NEWS_FAILURE = 'news/LOAD_NEWS_FAILURE';
+
+export const loadNews = createAction(LOAD_NEWS, weather => weather);
+function* loadNewsSaga(action) {
+    try {
+        const response = yield call(loadNewsAPI.loadNews, action.payload);
+        yield put({
+            type: LOAD_NEWS_SUCCESS,
+            payload: response.data
+        });
+    } catch (e) {
+        yield put({
+            type: LOAD_NEWS_FAILURE,
+            error: true,
+            payload: e
+        });
+    }
+}
+export function* newsSaga(){
+    yield takeLatest(LOAD_NEWS, loadNewsSaga);
+};
+
+const initialState = {
+    newsData: {
+        title: '',
+        originalLink: '',
+        link: '',
+        description: '',
+        pubDate: ''
+    },
+    error: null
+};
+
+const news = handleActions({
+    [LOAD_NEWS_SUCCESS]: (state, action) => ({
+        ...state,
+        newsData: action.payload,
+        error: null
+    }),
+    [LOAD_NEWS_FAILURE]: (state, action) => ({
+        ...state,
+        newsData: null,
+        error: action.payload
+    }),
+}, initialState);
+
+export default news;
