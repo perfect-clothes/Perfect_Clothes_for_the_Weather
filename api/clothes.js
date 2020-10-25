211d067cef6560ba92c0b6ac8e6eb8e45d73c0e2
JoieHuiju <joie.huiju@gmail.com>
2020-10-25 17:57:15 +0900

    [Update] Response random clothes data
    - DB에서 찾은 옷 데이터 중, 옷 타입이 겹치는 경우 임의의 것으로 반환

--- api/clothes.js
+++ api/clothes.js
@@ -17,23 +17,41 @@ exports.clothesLookup = (req, res, next) => {
             return res.status(400).json({err: err});    
         }
 
+        let outer = [];
+        let top = [];
+        let bottom = [];
+        let item = [];
+        let inner = [];
+
         for (let i = 0; i < result.length; i++) {
             if (result[i].clothes_type == 'outer') {
-                clothesData.outer = result[i].clothes_name;
+                outer.push(result[i].clothes_name);
             }
             else if (result[i].clothes_type == 'top') {
-                clothesData.top = result[i].clothes_name;
+                top.push(result[i].clothes_name);
             }
             else if (result[i].clothes_type == 'bottom') {
-                clothesData.bottom = result[i].clothes_name;
+                bottom.push(result[i].clothes_name);
             }
             else if (result[i].clothes_type == 'item') {
-                clothesData.item = result[i].clothes_name;
+                item.push(result[i].clothes_name);
             }
             else if (result[i].clothes_type == 'inner') {
-                clothesData.inner = result[i].clothes_name;
+                inner.push(result[i].clothes_name);
             }
         } // for
+
+        console.log(outer);
+        console.log(top);
+        console.log(bottom);
+        console.log(item);
+        console.log(inner);
+
+        clothesData.outer = outer[Math.floor(Math.random()*outer.length)];
+        clothesData.top = top[Math.floor(Math.random()*top.length)];
+        clothesData.bottom = bottom[Math.floor(Math.random()*bottom.length)];
+        clothesData.item = item[Math.floor(Math.random()*item.length)];
+        clothesData.inner = inner[Math.floor(Math.random()*inner.length)];
 
         return res.status(200).json({
             weatherData: req.weatherData,
