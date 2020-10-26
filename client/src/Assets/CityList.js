28cd0c1a5439131a5b1275cc6fdf9a146c48af33
zooneon <snsxhdz@naver.com>
2020-10-27 00:22:12 +0900

    [Etc] Update city list

--- client/src/Assets/CityList.js
+++ client/src/Assets/CityList.js
@@ -94,6 +94,836 @@ const CityList = [
         name: '울산',
         latitude: 35.566669,
         longitude: 129.266663
+    },
+    {
+        name: 'Cheonan',
+        latitude: 36.806499,
+        longitude: 127.152199
+    },
+    {
+        name: 'cheonan',
+        latitude: 36.806499,
+        longitude: 127.152199
+    },
+    {
+        name: '천안',
+        latitude: 36.806499,
+        longitude: 127.152199
+    },
+    {
+        name: 'Goyang',
+        latitude: 37.656391,
+        longitude: 126.323608
+    },
+    {
+        name: 'goyang',
+        latitude: 37.656391,
+        longitude: 126.323608
+    },
+    {
+        name: '고양',
+        latitude: 37.656391,
+        longitude: 126.323608
+    },
+    {
+        name: 'Gwacheon',
+        latitude: 37.42889,
+        longitude: 126.989166
+    },
+    {
+        name: 'gwacheon',
+        latitude: 37.42889,
+        longitude: 126.989166
+    },
+    {
+        name: '과천',
+        latitude: 37.42889,
+        longitude: 126.989166
+    },
+    {
+        name: 'Cheongju',
+        latitude: 36.637218,
+        longitude: 127.489723
+    },
+    {
+        name: 'cheongju',
+        latitude: 36.637218,
+        longitude: 127.489723
+    },
+    {
+        name: '청주',
+        latitude: 36.637218,
+        longitude: 127.489723
+    },
+    {
+        name: 'Jeonju',
+        latitude: 35.821941,
+        longitude: 127.148888
+    },
+    {
+        name: 'jeonju',
+        latitude: 35.821941,
+        longitude: 127.148888
+    },
+    {
+        name: '전주',
+        latitude: 35.821941,
+        longitude: 127.148888
+    },
+    {
+        name: 'Chuncheon',
+        latitude: 37.874722,
+        longitude: 127.734169
+    },
+    {
+        name: 'chuncheon',
+        latitude: 37.874722,
+        longitude: 127.734169
+    },
+    {
+        name: '춘천',
+        latitude: 37.874722,
+        longitude: 127.734169
+    },
+    {
+        name: 'Chungju',
+        latitude: 36.970558,
+        longitude: 127.93222
+    },
+    {
+        name: 'chungju',
+        latitude: 36.970558,
+        longitude: 127.93222
+    },
+    {
+        name: '충주',
+        latitude: 36.970558,
+        longitude: 127.93222
+    },
+    {
+        name: 'Hwaseong',
+        latitude: 37.206821,
+        longitude: 126.816902
+    },
+    {
+        name: 'hwaseong',
+        latitude: 37.206821,
+        longitude: 126.816902
+    },
+    {
+        name: '화성',
+        latitude: 37.206821,
+        longitude: 126.816902
+    },
+    {
+        name: 'Icheon',
+        latitude: 37.279171,
+        longitude: 127.442497
+    },
+    {
+        name: 'icheon',
+        latitude: 37.279171,
+        longitude: 127.442497
+    },
+    {
+        name: '이천',
+        latitude: 37.279171,
+        longitude: 127.442497
+    },
+    {
+        name: 'Iksan',
+        latitude: 35.94389,
+        longitude: 126.954437
+    },
+    {
+        name: 'iksan',
+        latitude: 35.94389,
+        longitude: 126.954437
+    },
+    {
+        name: '익산',
+        latitude: 35.94389,
+        longitude: 126.954437
+    },
+    {
+        name: 'Gimcheon',
+        latitude: 36.121761,
+        longitude: 128.119812
+    },
+    {
+        name: 'gimcheon',
+        latitude: 36.121761,
+        longitude: 128.119812
+    },
+    {
+        name: '김천',
+        latitude: 36.121761,
+        longitude: 128.119812
+    },
+    {
+        name: 'Kimhae',
+        latitude: 35.234169,
+        longitude: 128.81104
+    },
+    {
+        name: 'kimhae',
+        latitude: 35.234169,
+        longitude: 128.81104
+    },
+    {
+        name: '김해',
+        latitude: 35.234169,
+        longitude: 128.81104
+    },
+    {
+        name: 'Kimje',
+        latitude: 35.80167,
+        longitude: 126.888893
+    },
+    {
+        name: 'kimje',
+        latitude: 35.80167,
+        longitude: 126.888893
+    },
+    {
+        name: '김제',
+        latitude: 35.80167,
+        longitude: 126.888893
+    },
+    {
+        name: 'Gimpo',
+        latitude: 37.623611,
+        longitude: 126.714172
+    },
+    {
+        name: 'gimpo',
+        latitude: 37.623611,
+        longitude: 126.714172
+    },
+    {
+        name: '김포',
+        latitude: 37.623611,
+        longitude: 126.714172
+    },
+    {
+        name: 'Gongju',
+        latitude: 36.455559,
+        longitude: 127.124718
+    },
+    {
+        name: 'gongju',
+        latitude: 36.455559,
+        longitude: 127.124718
+    },
+    {
+        name: '공주',
+        latitude: 36.455559,
+        longitude: 127.124718
+    },
+    {
+        name: 'Gumi',
+        latitude: 36.113602,
+        longitude: 128.335999
+    },
+    {
+        name: 'gumi',
+        latitude: 36.113602,
+        longitude: 128.335999
+    },
+    {
+        name: '구미',
+        latitude: 36.113602,
+        longitude: 128.335999
+    },
+    {
+        name: 'Gunpo',
+        latitude: 37.3675,
+        longitude: 126.946938
+    },
+    {
+        name: 'gunpo',
+        latitude: 37.3675,
+        longitude: 126.946938
+    },
+    {
+        name: '군포',
+        latitude: 37.3675,
+        longitude: 126.946938
+    },
+    {
+        name: 'Gunsan',
+        latitude: 35.978611,
+        longitude: 126.711388
+    },
+    {
+        name: 'gunsan',
+        latitude: 35.978611,
+        longitude: 126.711388
+    },
+    {
+        name: '군산',
+        latitude: 35.978611,
+        longitude: 126.711388
+    },
+    {
+        name: 'Guri',
+        latitude: 37.598598,
+        longitude: 127.139397
+    },
+    {
+        name: 'guri',
+        latitude: 37.598598,
+        longitude: 127.139397
+    },
+    {
+        name: '구리',
+        latitude: 37.598598,
+        longitude: 127.139397
+    },
+    {
+        name: 'Gyeongju',
+        latitude: 35.842781,
+        longitude: 129.21167
+    },
+    {
+        name: 'gyeongju',
+        latitude: 35.842781,
+        longitude: 129.21167
+    },
+    {
+        name: '경주',
+        latitude: 35.842781,
+        longitude: 129.21167
+    },
+    {
+        name: 'Gyeongsan',
+        latitude: 35.82333,
+        longitude: 128.737778
+    },
+    {
+        name: 'gyeongsan',
+        latitude: 35.82333,
+        longitude: 128.737778
+    },
+    {
+        name: '경산',
+        latitude: 35.82333,
+        longitude: 128.737778
+    },
+    {
+        name: 'Miryang',
+        latitude: 35.493328,
+        longitude: 128.748886
+    },
+    {
+        name: 'miryang',
+        latitude: 35.493328,
+        longitude: 128.748886
+    },
+    {
+        name: '밀양',
+        latitude: 35.493328,
+        longitude: 128.748886
+    },
+    {
+        name: 'Mokpo',
+        latitude: 34.79361,
+        longitude: 126.388611
+    },
+    {
+        name: 'mokpo',
+        latitude: 34.79361,
+        longitude: 126.388611
+    },
+    {
+        name: '목포',
+        latitude: 34.79361,
+        longitude: 126.388611
+    },
+    {
+        name: 'Paju',
+        latitude: 37.761108,
+        longitude: 126.775002
+    },
+    {
+        name: 'paju',
+        latitude: 37.761108,
+        longitude: 126.775002
+    },
+    {
+        name: '파주',
+        latitude: 37.761108,
+        longitude: 126.775002
+    },
+    {
+        name: 'Mungyeong',
+        latitude: 36.594582,
+        longitude: 128.199463
+    },
+    {
+        name: 'mungyeong',
+        latitude: 36.594582,
+        longitude: 128.199463
+    },
+    {
+        name: '문경',
+        latitude: 36.594582,
+        longitude: 128.199463
+    },
+    {
+        name: 'Naju',
+        latitude: 35.028332,
+        longitude: 126.717499
+    },
+    {
+        name: 'naju',
+        latitude: 35.028332,
+        longitude: 126.717499
+    },
+    {
+        name: '나주',
+        latitude: 35.028332,
+        longitude: 126.717499
+    },
+    {
+        name: 'Nonsan',
+        latitude: 36.203892,
+        longitude: 127.084717
+    },
+    {
+        name: 'nonsan',
+        latitude: 36.203892,
+        longitude: 127.084717
+    },
+    {
+        name: '논산',
+        latitude: 36.203892,
+        longitude: 127.084717
+    },
+    {
+        name: 'Asan',
+        latitude: 36.783611,
+        longitude: 127.004173
+    },
+    {
+        name: 'asan',
+        latitude: 36.783611,
+        longitude: 127.004173
+    },
+    {
+        name: '아산',
+        latitude: 36.783611,
+        longitude: 127.004173
+    },
+    {
+        name: 'Osan',
+        latitude: 37.152222,
+        longitude: 127.070557
+    },
+    {
+        name: 'osan',
+        latitude: 37.152222,
+        longitude: 127.070557
+    },
+    {
+        name: '오산',
+        latitude: 37.152222,
+        longitude: 127.070557
+    },
+    {
+        name: 'Pohang',
+        latitude: 36.032219,
+        longitude: 129.365005
+    },
+    {
+        name: 'pohang',
+        latitude: 36.032219,
+        longitude: 129.365005
+    },
+    {
+        name: '포항',
+        latitude: 36.032219,
+        longitude: 129.365005
+    },
+    {
+        name: 'Bucheon',
+        latitude: 37.49889,
+        longitude: 126.783058
+    },
+    {
+        name: 'bucheon',
+        latitude: 37.49889,
+        longitude: 126.783058
+    },
+    {
+        name: '부천',
+        latitude: 37.49889,
+        longitude: 126.783058
+    },
+    {
+        name: 'Pyeongtaek',
+        latitude: 36.99472,
+        longitude: 127.08889
+    },
+    {
+        name: 'pyeongtaek',
+        latitude: 36.99472,
+        longitude: 127.08889
+    },
+    {
+        name: '평택',
+        latitude: 36.99472,
+        longitude: 127.08889
+    },
+    {
+        name: 'Sangju',
+        latitude: 36.415279,
+        longitude: 128.160553
+    },
+    {
+        name: 'sangju',
+        latitude: 36.415279,
+        longitude: 128.160553
+    },
+    {
+        name: '상주',
+        latitude: 36.415279,
+        longitude: 128.160553
+    },
+    {
+        name: 'Sokcho',
+        latitude: 38.208328,
+        longitude: 128.59111
+    },
+    {
+        name: 'sokcho',
+        latitude: 38.208328,
+        longitude: 128.59111
+    },
+    {
+        name: '속초',
+        latitude: 38.208328,
+        longitude: 128.59111
+    },
+    {
+        name: 'Seosan',
+        latitude: 36.78167,
+        longitude: 126.452217
+    },
+    {
+        name: 'seosan',
+        latitude: 36.78167,
+        longitude: 126.452217
+    },
+    {
+        name: '서산',
+        latitude: 36.78167,
+        longitude: 126.452217
+    },
+    {
+        name: 'Suncheon',
+        latitude: 34.948078,
+        longitude: 127.489471
+    },
+    {
+        name: 'suncheon',
+        latitude: 34.948078,
+        longitude: 127.489471
+    },
+    {
+        name: '순천',
+        latitude: 34.948078,
+        longitude: 127.489471
+    },
+    {
+        name: 'Suwon',
+        latitude: 37.291111,
+        longitude: 127.008888
+    },
+    {
+        name: 'suwon',
+        latitude: 37.291111,
+        longitude: 127.008888
+    },
+    {
+        name: '수원',
+        latitude: 37.291111,
+        longitude: 127.008888
+    },
+    {
+        name: 'Taebaek',
+        latitude: 37.1759,
+        longitude: 128.988907
+    },
+    {
+        name: 'taebaek',
+        latitude: 37.1759,
+        longitude: 128.988907
+    },
+    {
+        name: '태백',
+        latitude: 37.1759,
+        longitude: 128.988907
+    },
+    {
+        name: 'Boryeong',
+        latitude: 36.349312,
+        longitude: 126.597717
+    },
+    {
+        name: 'boryeong',
+        latitude: 36.349312,
+        longitude: 126.597717
+    },
+    {
+        name: '보령',
+        latitude: 36.349312,
+        longitude: 126.597717
+    },
+    {
+        name: 'Uijeongbu',
+        latitude: 37.741501,
+        longitude: 127.047401
+    },
+    {
+        name: 'uijeongbu',
+        latitude: 37.741501,
+        longitude: 127.047401
+    },
+    {
+        name: '의정부',
+        latitude: 37.741501,
+        longitude: 127.047401
+    },
+    {
+        name: 'Wonju',
+        latitude: 37.351391,
+        longitude: 127.945282
+    },
+    {
+        name: 'wonju',
+        latitude: 37.351391,
+        longitude: 127.945282
+    },
+    {
+        name: '원주',
+        latitude: 37.351391,
+        longitude: 127.945282
+    },
+    {
+        name: 'Yangju',
+        latitude: 37.833111,
+        longitude: 127.061691
+    },
+    {
+        name: 'yangju',
+        latitude: 37.833111,
+        longitude: 127.061691
+    },
+    {
+        name: '양주',
+        latitude: 37.833111,
+        longitude: 127.061691
+    },
+    {
+        name: 'Yangsan',
+        latitude: 35.338612,
+        longitude: 129.038605
+    },
+    {
+        name: 'yangsan',
+        latitude: 35.338612,
+        longitude: 129.038605
+    },
+    {
+        name: '양산',
+        latitude: 35.338612,
+        longitude: 129.038605
+    },
+    {
+        name: 'Yeoju',
+        latitude: 37.29583,
+        longitude: 127.633888
+    },
+    {
+        name: 'yeoju',
+        latitude: 37.29583,
+        longitude: 127.633888
+    },
+    {
+        name: '여주',
+        latitude: 37.29583,
+        longitude: 127.633888
+    },
+    {
+        name: 'Yongin',
+        latitude: 37.234169,
+        longitude: 127.20639
+    },
+    {
+        name: 'yongin',
+        latitude: 37.234169,
+        longitude: 127.20639
+    },
+    {
+        name: '용인',
+        latitude: 37.234169,
+        longitude: 127.20639
+    },
+    {
+        name: 'Jeju',
+        latitude: 33.50972,
+        longitude: 126.521942
+    },
+    {
+        name: 'jeju',
+        latitude: 33.50972,
+        longitude: 126.521942
+    },
+    {
+        name: '제주',
+        latitude: 33.50972,
+        longitude: 126.521942
+    },
+    {
+        name: 'Changwon',
+        latitude: 35.228062,
+        longitude: 128.681107
+    },
+    {
+        name: 'changwon',
+        latitude: 35.228062,
+        longitude: 128.681107
+    },
+    {
+        name: '창원',
+        latitude: 35.228062,
+        longitude: 128.681107
+    },
+    {
+        name: 'Anyang',
+        latitude: 37.392502,
+        longitude: 126.926941
+    },
+    {
+        name: 'Anyang',
+        latitude: 37.392502,
+        longitude: 126.926941
+    },
+    {
+        name: 'anyang',
+        latitude: 37.392502,
+        longitude: 126.926941
+    },
+    {
+        name: '안양',
+        latitude: 37.392502,
+        longitude: 126.926941
+    },
+    {
+        name: 'Anseong',
+        latitude: 37.01083,
+        longitude: 127.270279
+    },
+    {
+        name: 'anseong',
+        latitude: 37.01083,
+        longitude: 127.270279
+    },
+    {
+        name: '안성',
+        latitude: 37.01083,
+        longitude: 127.270279
+    },
+    {
+        name: 'Ansan',
+        latitude: 37.323608,
+        longitude: 126.821938
+    },
+    {
+        name: 'ansan',
+        latitude: 37.323608,
+        longitude: 126.821938
+    },
+    {
+        name: '안산',
+        latitude: 37.323608,
+        longitude: 126.821938
+    },
+    {
+        name: 'Andong',
+        latitude: 36.565559,
+        longitude: 128.725006
+    },
+    {
+        name: 'andong',
+        latitude: 36.565559,
+        longitude: 128.725006
+    },
+    {
+        name: '안동',
+        latitude: 36.565559,
+        longitude: 128.725006
+    },
+    {
+        name: 'Seongnam',
+        latitude: 37.43861,
+        longitude: 127.137779
+    },
+    {
+        name: 'seongnam',
+        latitude: 37.43861,
+        longitude: 127.137779
+    },
+    {
+        name: '성남',
+        latitude: 37.43861,
+        longitude: 127.137779
+    },
+    {
+        name: 'Hanam',
+        latitude: 37.540001,
+        longitude: 127.205559
+    },
+    {
+        name: 'hanam',
+        latitude: 37.540001,
+        longitude: 127.205559
+    },
+    {
+        name: '하남',
+        latitude: 37.540001,
+        longitude: 127.205559
+    },
+    {
+        name: 'Namyangju',
+        latitude: 37.636669,
+        longitude: 127.214172
+    },
+    {
+        name: 'namyangju',
+        latitude: 37.636669,
+        longitude: 127.214172
+    },
+    {
+        name: '남양주',
+        latitude: 37.636669,
+        longitude: 127.214172
+    },
+    {
+        name: 'Gangneung',
+        latitude: 37.755562,
+        longitude: 128.896103
+    },
+    {
+        name: 'gangneung',
+        latitude: 37.755562,
+        longitude: 128.896103
+    },
+    {
+        name: '강릉',
+        latitude: 37.755562,
+        longitude: 128.896103
     }
 ];
 
